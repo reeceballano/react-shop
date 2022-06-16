e3cb04b16440501321c648052fe85f93dd446938
Reece <trazhme@gmail.com>
2022-06-16 19:10:23 +0800

    added increase decrease function

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,15 +1,31 @@
+import { useContext, useState } from "react";
+import DataContext from "../context/ProductAPI";
 import Currency from "./Currency";
 
 const CartItem = ({item}) => {
-    const { id, name, price, qty } = item;
-    
-    const handleIncreaseItem = () => {
-        console.log('increase cart item');
+    const  { id, name, price, qty } = item;
+    const { products } = useContext(DataContext);
 
+    const [cartQty, setCartQty] = useState(qty);
+
+    const handleIncreaseItem = () => {
+        console.log('increase cart item', qty);
+
+        const product = products.find(item => item.id === id);
+
+        if(cartQty <= product.stocks) {
+            console.log('add qty');
+            setCartQty(cartQty+1);
+        }
     }
 
     const handleDecreaseItem = () => {
         console.log('decrease cart item');
+
+        if(cartQty !== 0) {
+            console.log('minus qty');
+            setCartQty(cartQty-1);
+        }
     }
 
     const total = () => {
@@ -18,7 +34,7 @@ const CartItem = ({item}) => {
 
     return(
         <div className="cart-item">
-            {id} - {name} <Currency />{price} {qty} | <Currency />{total()}
+            {id} - {name} <Currency />{price} {cartQty} | <Currency />{total()}
             <button onClick={handleIncreaseItem}>+</button>
             <button onClick={handleDecreaseItem}>-</button>
         </div>
